0535561c8ee035df66f906b27a48f3b88d781e61
calixlock <calixlock@naver.com>
2023-07-07 15:42:46 +0900

    êbackend에서 가져온 데이터ë 갯수만큰 만들얁¼지도록 로직변경

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -5,7 +5,8 @@
       <div class="row py-lg-5">
         <div class="col-lg-6 col-md-8 mx-auto">
           <h1 class="fw-light">Album example</h1>
-          <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+          <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the
+            creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
           <p>
             <a href="#" class="btn btn-primary my-2">Main call to action</a>
             <a href="#" class="btn btn-secondary my-2">Secondary action</a>
@@ -18,8 +19,9 @@
       <div class="container">
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col" v-for="i in 12" :key="i">
-            <Card/>
+          <div class="col" v-for="(item,idx) in state.items" :key="idx">
+            {{ item }}
+<!--            <Card : item="item"/>-->
           </div>
 
         </div>
@@ -31,14 +33,24 @@
 <script>
 import Card from "@/components/Card.vue";
 import axios from "axios";
+import {reactive} from "vue";
 
 export default {
   name: "Home",
   components: {Card},
-  setup(){
-    axios.get("/api/items").then((res)=>{
-      console.log(res);
+  setup() {
+    const state = reactive({
+      items: []
     })
+
+    // axios.get("/api/items").then((res)=>{
+    // console.log(res);
+    // state.items = res.data;
+    axios.get("/api/items").then(({data}) => { //res의 특정 값을 바로 가져올수 있다.(res.data) >> ({data})
+      state.items = data
+
+    })
+    return {state}
   }
 
 }
