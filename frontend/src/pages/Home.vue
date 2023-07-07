4b439a15e91c869a7e5687e8a6ff229953e710fc
calixlock <calixlock@naver.com>
2023-07-07 10:36:53 +0900

    forë ë반복문으로 일단 12개 생성

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -18,7 +18,7 @@
       <div class="container">
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col">
+          <div class="col" v-for="i in 12" :key="i">
             <Card/>
           </div>
 
