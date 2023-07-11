b7a49fde52a64a006b55f377bd650b8a060ca6fb
calixlock <calixlock@naver.com>
2023-07-11 23:57:18 +0900

    setting_reload

--- backend/src/main/java/org/amj/gallery/backend/entity/Item.java
+++ backend/src/main/java/org/amj/gallery/backend/entity/Item.java
@@ -14,4 +14,11 @@ public class Item {
     @Column(length = 50, nullable = false) // 문장길이, null  허용x 설정 적용
     private String name;
 
+    @Column(length = 100) // db  추가 항목 설정 적용
+    private String imgPath;
+    @Column
+    private int price;
+    @Column
+    private int discountPer; // discount_per >> discountPer 호환됨
+
 }
--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="card shadow-sm">
-    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+<!--    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
+    <span class="img" :style="{backgroundImage: `url(${item.imgPath})`}" />
+    <img :src="item.imgPath" />
     <div class="card-body">
-      <p class="card-text"> {{item}} </p>
+      <p class="card-text"> {{item.name}} </p>
       <div class="d-flex justify-content-between align-items-center">
         <div class="btn-group">
-          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+<!--          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>-->
+<!--          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>-->
+          <button class="btn btn-primary">구입하기</button>
         </div>
-        <small class="text-body-secondary">9 mins</small>
+        <small class="text-muted">
+          {{item.price}}원
+        </small>
       </div>
     </div>
   </div>
@@ -17,10 +22,16 @@
 export default {
   name : "Card",
   props : {
-    item: String
+    item: Object // 백엔드에서 가져온 dataType 지정
   }
 }
 </script>
 <style scoped>
-
+.card .img{
+  display: inline-block;
+  width: 100%;
+  height: 250px;
+  background-size: contain;
+  background-position: center;
+}
 </style>
