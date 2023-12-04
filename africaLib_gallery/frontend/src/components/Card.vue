99a67707ef084c6a9b9380ca4a649cb22e42e2b4
calixlock <calixlock@naver.com>
2023-12-04 23:51:46 +0900

    vue-test중

--- africaLib_gallery/frontend/src/components/Card.vue
+++ africaLib_gallery/frontend/src/components/Card.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="card shadow-sm">
-    <span class="img" :style="{backgroundImage: `url(${item.imgPath})`}" />
-<!--    <img :src="item.imgPath" />-->
+    <span class="img" :style="{ backgroundImage: `url(${item.imgPath})` }" />
+    <!--    <img :src="item.imgPath" />-->
     <div class="card-body">
       <p class="card-text">
         <span class="discount badge bg-danger">
-          <div class="discount-text fs-7">
-            -{{item.discountPer}}%
-          </div>
+          <div class="discount-text fs-7">-{{ item.discountPer }}%</div>
         </span>
-        &nbsp;{{item.name}}
+        &nbsp;{{ item.name }}
       </p>
       <div class="d-flex justify-content-between align-items-center">
-<!--        <div class="btn-group">-->
-<!--          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>-->
-<!--          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>-->
-          <button class="btn btn-primary">구입하기</button>
-<!--        </div>-->
+        <!--        <div class="btn-group">-->
+        <!--          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>-->
+        <!--          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>-->
+        <button class="btn btn-primary">구입하기</button>
+        <!--        </div>-->
         <small class="price text-muted">
-          {{lib.getNumberFormatted(item.price)}}
+          {{ lib.getNumberFormatted(item.price) }}
         </small>
 
         <small class="realPrice fs-5">
-          {{lib.getNumberFormatted(item.price*(100-item.discountPer)/100)}}
+          {{
+            lib.getNumberFormatted(
+              (item.price * (100 - item.discountPer)) / 100
+            )
+          }}
         </small>
       </div>
     </div>
@@ -32,28 +34,29 @@
 import lib from "@/scripts/lib";
 
 export default {
-  name : "Card",
-  props : {
+  name: "Card",
+  props: {
     // item: String
-    item: Object // 백엔드에서 가져온 dataType 지정
+    item: Object, // 백엔드에서 가져온 dataType 지정
   },
-  setup(){
-    return {lib}
-  }
-}
+  setup() {
+    return { lib };
+  },
+};
 </script>
 <style scoped>
-.card .img{
+.card .img {
   display: inline-block;
   width: 100%;
   height: 250px;
-  background : no-repeat center;
+  background: no-repeat center;
   background-size: contain;
 
-  //background-size: cover;  //object-fit:cover;
-  //background-position: center;
+  /* //background-size: cover;
+  //object-fit:cover;
+  //background-position: center; */
 }
-.card .card-body .price{
+.card .card-body .price {
   text-decoration: line-through;
 }
 </style>
