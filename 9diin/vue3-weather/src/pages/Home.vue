8f71bde40c5ea44702f475ca3545afbcafe24c6d
calixlock <calixlock@naver.com>
2024-02-05 15:32:17 +0900

    home의 page에 scss style 지정

--- 9diin/vue3-weather/src/pages/Home.vue
+++ 9diin/vue3-weather/src/pages/Home.vue
@@ -1,7 +1,30 @@
 <template>
-  <h1>Home.vue</h1>
+  <div class="wrap">
+    <div class="container">aa</div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<!-- css 문법스타일 : scss / scoped : 현재 위치만 스타일 적용 -->
+<style lang="scss" scoped>
+// @import "~/scss/main.scss";
+@import "../scss/main.scss";
+
+.wrap {
+  @include center;
+  width: 100%;
+  height: 100vh;
+  font-size: 16px;
+
+  .container {
+    @include center-sb;
+    width: 55%;
+    height: 90%;
+    max-width: 1920px;
+    max-height: 1354px;
+  }
+}
+</style>
