a57d9e48b313d210f047deb35caa47836416449b
calixlock <calixlock@naver.com>
2024-03-12 16:35:45 +0900

    declarative rendering test

--- 9diin/vue-basic/src/App.vue
+++ 9diin/vue-basic/src/App.vue
@@ -1,34 +1,15 @@
 <template>
-  <div>{{ count }}</div>
-  <h1>Vue_js lifeCycle testing</h1>
+  <div>{{ rawHtml }}</div>
+  <div>{{ rawHtml2 }}</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      count: 0,
+      // 텍스트 보간법을 이용한 선언적 렌더링
+      rawHtml: "텍스트 입니다",
     };
-  },
-  // life cycle test
-  beforeCreate() {
-    console.log("lifecycle is beforeCreate : ", this.count);
-    // this.test(); // 호출 에러 발생
-  },
-  created() {
-    console.log("lifecycle is created : ", this.count);
-    this.test();
-  },
-  // beforeMount() {
-  //   console.log("lifecycle is beforeMount : ", document.querySelector("h1"));
-  // },
-  // mounted() {
-  //   console.log("lifecycle is mounted : ", document.querySelector("h1"));
-  // },
-  methods: {
-    test() {
-      console.log("test function 호출");
-    },
   },
 };
 </script>
