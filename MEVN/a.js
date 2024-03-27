30e4165369afa8507b9c0d834d933d03aed1758b
calixlock <calixlock@naver.com>
2024-03-27 14:21:59 +0900

    js_module

---
+++ MEVN/a.js
@@ -0,0 +1,5 @@
+const s = 10;
+export default s;
+export const s1 = 1;
+export const s2 = 2;
+export const s3 = 3;
---
+++ MEVN/b.js
@@ -0,0 +1,6 @@
+const ll = console.log;
+import a from "./a.js";
+ll(a);
+
+import { s1, s2, s3 } from "./a.js";
+ll(`s1,s2,s3 : ${s1}, ${s2}, ${s3}`);
