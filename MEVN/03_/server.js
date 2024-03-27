ed31b02f277dca854df3ef2eee303f8e4e090f31
calixlock <calixlock@naver.com>
2024-03-27 17:09:27 +0900

    03_express start

---
+++ MEVN/03_/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const _path = path.join(__dirname, "./dist");
+app.use("/", express.static(_path));
+app.listen(12010, () => {
+  console.log("server: start | port: 12010");
+});
